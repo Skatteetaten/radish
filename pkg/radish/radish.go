c648fb909ffc4e19e5c91dd42b5d54f2d6e5d5ec
Frode Laukus <frode.laukus@skatteetaten.no>
2018-06-04 12:13:21 +0200

    AOT-177 trying bash

--- pkg/radish/radish.go
+++ pkg/radish/radish.go
@@ -2,6 +2,7 @@ package radish
 
 import (
 	"os"
+	"os/exec"
 	"os/signal"
 	"syscall"
 
@@ -13,21 +14,26 @@ import (
 
 //RunRadish : main executor for Radish
 func RunRadish(args []string) {
-	e := executor.NewJavaExecutor(executor.FAILSAFE)
-	cmd := e.Execute(args)
-	err := cmd.Start()
-	if err != nil {
-		logrus.Fatalf("Error starting: %s", err)
+	if args[0] == "bash" {
+		exec.Command("/bin/bash")
+	} else {
+		e := executor.NewJavaExecutor(executor.FAILSAFE)
+		cmd := e.Execute(args)
+		err := cmd.Start()
+		if err != nil {
+			logrus.Fatalf("Error starting: %s", err)
+		}
+		reaper.Start()
+		signal.Ignore(syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGKILL)
+		pid := cmd.Process.Pid
+		signaler.Start(cmd.Process)
+		var wstatus syscall.WaitStatus
+		//processState, error := cmd.Process.Wait()
+		//processState.
+		syscall.Wait4(int(pid), &wstatus, 0, nil)
+		logrus.Infof("Exit code %d", wstatus.ExitStatus())
+		exitCode := e.HandleExit(wstatus.ExitStatus(), pid)
+		os.Exit(int(exitCode))
 	}
-	reaper.Start()
-	signal.Ignore(syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGKILL)
-	pid := cmd.Process.Pid
-	signaler.Start(cmd.Process)
-	var wstatus syscall.WaitStatus
-	//processState, error := cmd.Process.Wait()
-	//processState.
-	syscall.Wait4(int(pid), &wstatus, 0, nil)
-	logrus.Infof("Exit code %d", wstatus.ExitStatus())
-	exitCode := e.HandleExit(wstatus.ExitStatus(), pid)
-	os.Exit(int(exitCode))
+
 }
